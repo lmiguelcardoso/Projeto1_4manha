edb90f42f2a0fe55a3888a8a1975795fa09d96b2
Miguel Cardoso <miguel.cardoso@avanade.com>
2022-09-16 14:37:30 -0300

    first commit

---
+++ dist/prova.js
@@ -0,0 +1,8 @@
+"use strict";
+for (let i = 1; i <= 10; i++) {
+    console.log(`Tabuada do ${i}`);
+    for (let x = 1; x <= 10; x++) {
+        console.log(`${i} x ${x} = `, i * x);
+    }
+    console.log('');
+}
---
+++ src/prova.ts
@@ -0,0 +1,7 @@
+for(let i:number = 1; i<=10; i++){
+    console.log(`Tabuada do ${i}`)
+    for(let x:number = 1; x<=10; x++){
+        console.log(`${i} x ${x} = `, i*x)
+    }
+    console.log('')
+}
